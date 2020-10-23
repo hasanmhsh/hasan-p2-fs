b2b74b4ae1f14d26df1df4ce67e8cd58e4b8fd8f
hasan-m.shehata <hasan.mh.sh@gmail.com>
2020-10-23 23:10:10 +0200

    Set up CORS. Allow * for origins

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -14,8 +14,16 @@ def create_app(test_config=None):
   setup_db(app)
   
   '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+  @DONE: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
+  CORS(app)
+
+  # CORS Headers 
+  @app.after_request
+  def after_request(response):
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+      response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+      return response
 
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
