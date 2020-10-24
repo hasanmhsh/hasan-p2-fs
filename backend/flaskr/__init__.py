9edfaa46a213ba585d6c120737f8cb74f1268a58
hasan-m.shehata <hasan.mh.sh@gmail.com>
2020-10-24 11:32:09 +0200

    Create an endpoint to handle GET requests for questions,   including pagination (every 10 questions).   This endpoint should return a list of questions,   number of total questions, current category, categories.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -68,7 +68,7 @@ def create_app(test_config=None):
   #     return 'done'
       
   '''
-  @TODO: 
+  @DONE: 
   Create an endpoint to handle GET requests for questions, 
   including pagination (every 10 questions). 
   This endpoint should return a list of questions, 
@@ -79,7 +79,59 @@ def create_app(test_config=None):
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+  @app.route('/questions')
+  def get_questions():
+      selection = Question.query.order_by(Question.id).all()
+      categories = Category.query.order_by(Category.id).all()
+      total_size = len(selection)
 
+      if total_size==0:
+          abort(404)
+      current_questions = paginate_questions(request, selection)
+      return jsonify({
+        "success": True,
+        "questions": current_questions,
+        # "questions": [question.question for question in current_questions],
+        "total_questions": total_size,
+        "categories": [cat.type for cat in categories ],
+        "current_category": current_questions[0]['category']
+      })
+
+  def paginate_questions(request, selection):
+      page = request.args.get('page', 1, type=int)
+      start = (page -1) * QUESTIONS_PER_PAGE
+      end = start + QUESTIONS_PER_PAGE
+      questions = [question.format() for question in selection]
+      current_questions = questions[start:end]
+      return current_questions
+      # return selection[start:end]
+
+  @app.route('/questions/create/debug-only')
+  def c_categories():
+      questions = Question.query.all()
+      for q in questions:
+        db.session.delete(q)
+      db.session.commit()
+      Question(question="question1?", answer="answer1", category="cat1", difficulty=10).insert()
+      Question(question="question2?", answer="answer2", category="cat2", difficulty=20).insert()
+      Question(question="question3?", answer="answer3", category="cat3", difficulty=30).insert()
+      Question(question="question4?", answer="answer4", category="cat4", difficulty=40).insert()
+      Question(question="question5?", answer="answer5", category="cat5", difficulty=50).insert()
+      Question(question="question6?", answer="answer6", category="cat6", difficulty=60).insert()
+      Question(question="question7?", answer="answer7", category="cat1", difficulty=70).insert()
+      Question(question="question8?", answer="answer8", category="cat2", difficulty=80).insert()
+      Question(question="question9?", answer="answer9", category="cat3", difficulty=90).insert()
+      Question(question="question10?", answer="answer10", category="cat4", difficulty=100).insert()
+      Question(question="question11?", answer="answer11", category="cat5", difficulty=110).insert()
+      Question(question="question12?", answer="answer12", category="cat6", difficulty=120).insert()
+      Question(question="question13?", answer="answer13", category="cat1", difficulty=130).insert()
+      Question(question="question14?", answer="answer14", category="cat2", difficulty=140).insert()
+      Question(question="question15?", answer="answer15", category="cat3", difficulty=150).insert()
+      Question(question="question16?", answer="answer16", category="cat4", difficulty=160).insert()
+      Question(question="question17?", answer="answer17", category="cat5", difficulty=170).insert()
+      Question(question="question18?", answer="answer18", category="cat6", difficulty=180).insert()
+      return 'done'
+    
   '''
   @TODO: 
   Create an endpoint to DELETE question using a question ID. 
